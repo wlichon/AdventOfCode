52b61dc67b8830cafead19f4c660b6df36ba87f2
Wiktor <wiki.lichon@gmail.com>
2023-12-08 20:55:15 +0100

    3rd day done

--- AoC/AoC3/Program.cs
+++ AoC/AoC3/Program.cs
@@ -90,61 +90,6 @@ namespace AoC3
            public int ContentToInt => int.Parse(content);
         }
     }
-
-    public class SolutionGit   
-    {
-
-        // Introduce a Parse function that returns the interesting 'blocks' of texts 
-        // and positions using a regex. Then just filter and match these according
-        // to the problem spec.
-
-        public object PartOne()
-        {
-            var rows = Resources.input.Split("\r\n");
-            var symbols = Parse(rows, new Regex(@"[^.0-9]"));
-            var nums = Parse(rows, new Regex(@"\d+"));
-
-            return (
-                from n in nums
-                where symbols.Any(s => Adjacent(s, n))
-                select n.Int
-            ).Sum();
-        }
-
-        public object PartTwo(string input)
-        {
-            var rows = input.Split("\n");
-            var gears = Parse(rows, new Regex(@"\*"));
-            var numbers = Parse(rows, new Regex(@"\d+"));
-
-            return (
-                from g in gears
-                let neighbours = from n in numbers where Adjacent(n, g) select n.Int
-                where neighbours.Count() == 2
-                select neighbours.First() * neighbours.Last()
-            ).Sum();
-        }
-
-        // checks that the parts are touching each other, i.e. rows are within 1 
-        // step and also the columns (using https://stackoverflow.com/a/3269471).
-        bool Adjacent(Part p1, Part p2) =>
-            Math.Abs(p2.Irow - p1.Irow) <= 1 &&
-            p1.Icol <= p2.Icol + p2.Text.Length &&
-            p2.Icol <= p1.Icol + p1.Text.Length;
-
-        // returns the matches of rx with its coordinates
-        Part[] Parse(string[] rows, Regex rx) => (
-            from irow in Enumerable.Range(0, rows.Length)
-            from match in rx.Matches(rows[irow])
-            select new Part(match.Value, irow, match.Index)
-        ).ToArray();
-    }
-
-    record Part(string Text, int Irow, int Icol)
-    {
-        public int Int => int.Parse(Text);
-    }
-
     public class Solution
     {
         private int _x;
@@ -388,12 +333,7 @@ namespace AoC3
             Console.WriteLine(sol.PartOne());
             sw.Stop();
             Console.WriteLine("Elapsed={0}", sw.Elapsed);
-            //Console.WriteLine(sol.PartTwo());
-            sw.Restart();
-            var solGit = new SolutionGit();
-            Console.WriteLine(solGit.PartOne());
-            sw.Stop();
-            Console.WriteLine("Elapsed={0}", sw.Elapsed);
+           
 
             sw.Restart();
             var solNew = new SolutionNew(input);
