e187c4d06782e2baef10ecb5905aafd76d9433cf
Wiktor <wiki.lichon@gmail.com>
2023-12-09 20:33:55 +0100

    aoc4 done

---
+++ AoC/AoC4/Program.cs
@@ -0,0 +1,109 @@
+﻿using AoC4.Properties;
+using System.Diagnostics.CodeAnalysis;
+using System.Text.RegularExpressions;
+
+namespace AoC4
+{
+    public class Solution
+    {
+        private string[] _input;
+        public Solution(string[] input)
+        {
+            _input = input;
+
+        }
+        public int PartOne()
+        {
+            int points = 0;
+            foreach(string game in _input)
+            {
+                points += CardPoints(game);
+            }
+
+            return points;
+        }
+
+        public long PartTwo()
+        {
+            long cards = 0;
+            Dictionary<int, int> indicesToCopies = new Dictionary<int, int>();
+
+            for(int i = 0; i < _input.Length; i++)
+            {
+                indicesToCopies[i] = 1;
+            }
+            for (int i = 0; i < _input.Length; i++)
+            {
+
+                int result = CardPoints(_input[i], secondPart:true);
+                for(int j = 1; j <= result; j++)
+                {
+                    if(j + i < _input.Length)
+                    {
+                        indicesToCopies[j + i] += indicesToCopies[i];
+                    }
+                    
+                }
+                cards += indicesToCopies[i];
+            }
+
+            return cards;
+        }
+
+        public int CardPoints(string card, bool secondPart = false)
+        {
+            int matched = 0;
+            string[] separated = card.Split("|");
+            List<int> winningNums = Parse(separated[0].Substring(separated[0].IndexOf(':')+1), @"(\d+)");
+            List<int> pickedNums = Parse(separated[1], @"(\d+)");
+
+            //HashSet<int> winningNums = new HashSet<int>();
+            
+            foreach(int num in pickedNums)
+            {
+                if (winningNums.Contains(num))
+                {
+                    matched++;
+                }
+            }
+            if (secondPart)
+            {
+                return matched;
+            }
+            return (int)Math.Pow(2, matched - 1);
+
+        }
+
+        /*
+        public List<int> Parse(string input, string rx)
+        {
+            return Regex.Matches(input, rx).Cast<Match>().Select(m => int.Parse(m.Value)).ToList();
+        }
+        */
+
+        public List<int> Parse(string input, string rx)
+        {
+            List<int> nums = new List<int>();
+            MatchCollection matches = Regex.Matches(input, rx);
+
+            foreach(Match match in matches)
+            {
+                nums.Add(int.Parse(match.Value));
+            }
+
+            return nums;
+        }
+
+    }
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var sol = new Solution(Resources.input.Split("\r\n"));
+            Console.WriteLine(sol.PartOne());
+            Console.WriteLine(sol.PartTwo());
+        }
+    }
+
+
+}
---
+++ AoC/TestProject1/Tests_AoC4.cs
@@ -0,0 +1,68 @@
+﻿using AoC4;
+
+namespace TestProject1
+{
+    public class Tests4
+    {
+
+
+        [Test]
+        public void Test1()
+        {
+            string[] input = {"Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53"};
+            Solution sol = new Solution(input);
+            int result = sol.PartOne();
+            Assert.That(result == 8);
+        }
+
+
+
+        [Test]
+        public void Test2()
+        {
+            string[] input = { "Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36" };
+            Solution sol = new Solution(input);
+            int result = sol.PartOne();
+            Assert.That(result == 0);
+        }
+
+        [Test]
+        public void Test3()
+        {
+            string[] input = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53\r\nCard 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19\r\nCard 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1\r\nCard 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83\r\nCard 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36\r\nCard 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11".Split("\r\n");
+            Solution sol = new Solution(input);
+            int result = sol.PartOne();
+            Assert.That(result == 13);
+        }
+
+
+        [Test]
+        public void Test4()
+        {
+            string[] input = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53\r\nCard 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19".Split("\r\n");
+            Solution sol = new Solution(input);
+            long result = sol.PartTwo();
+            Assert.That(result == 3);
+        }
+
+        [Test]
+        public void Test5()
+        {
+            string[] input = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53\r\nCard 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19\r\nCard 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1".Split("\r\n");
+            Solution sol = new Solution(input);
+            long result = sol.PartTwo();
+            Assert.That(result == 7);
+        }
+
+        [Test]
+        public void Test6()
+        {
+            string[] input = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53\r\nCard 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19\r\nCard 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1\r\nCard 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83\r\nCard 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36\r\nCard 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11".Split("\r\n");
+            Solution sol = new Solution(input);
+            long result = sol.PartTwo();
+            Assert.That(result == 30);
+        }
+
+
+    }
+}
